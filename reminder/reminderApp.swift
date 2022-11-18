4b27e4d5876dab2918085a781855a12b66bc753f
Ilona Sellenberk <ilona.sell@gmail.com>
2022-11-18 17:36:32 +0100

    initiate project reminder

---
+++ reminder/reminderApp.swift
@@ -0,0 +1,20 @@
+//
+//  reminderApp.swift
+//  reminder
+//
+//  Created by Ilona Sellenberkova on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct reminderApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
