9abf557f15cd546346c058b9e87aa5026100edef
Ilona Sellenberk <ilona.sell@gmail.com>
2022-11-21 11:50:19 +0100

    set up List table in core data, clean up previous default code

--- reminder/ContentView.swift
+++ reminder/ContentView.swift
@@ -12,46 +12,47 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+        sortDescriptors: [NSSortDescriptor()], animation: .default)
+    
+    private var allLists: FetchedResults<Listing>
 
     var body: some View {
         NavigationView {
+            
             List {
-                ForEach(items) { item in
+                ForEach(allLists) { list in
+                    
                     NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                        // When clicked on link -> the other screen content
+                        Text("List at....")
                     } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+                        Text(list.name!)
                     }
                 }
                 .onDelete(perform: deleteItems)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+//            .toolbar {
+//                ToolbarItem {
+//                    Button(action: addList) {
+//                        Label("Add List", systemImage: "plus")
+//                    }
+//                }
+//            }
+            
+            Text("Select an List")
+            
         }
     }
 
-    private func addItem() {
+    private func addList() {
         withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+            let newList = Listing(context: viewContext)
+            newList.name = "Hi"
 
             do {
                 try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            }
+            catch {
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
@@ -60,13 +61,12 @@ struct ContentView: View {
 
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
+            offsets.map { allLists[$0] }.forEach(viewContext.delete)
 
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+             
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
@@ -74,15 +74,10 @@ struct ContentView: View {
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
+
+// fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
